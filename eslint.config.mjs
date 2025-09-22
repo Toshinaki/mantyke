b8c99e67ae3c5dbd57216da133d5a5b492030e2e
Toshinaki <shaduking@hotmail.com>
2025-09-23 02:09:08 +0900

    fix: commands & workflows

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,10 +2,18 @@ import mantine from 'eslint-config-mantine';
 import tseslint from 'typescript-eslint';
 
 export default [
+  {
+    ignores: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/.next/**',
+      '**/out/**',
+      '**/*.{mjs,cjs,js,d.ts,d.mts}',
+    ],
+  },
   ...tseslint.configs.recommended,
   ...mantine,
   {
-    ignores: ['**/*.{mjs,cjs,js,d.ts,d.mts}'],
     languageOptions: {
       parserOptions: {
         tsconfigRootDir: import.meta.dirname,
